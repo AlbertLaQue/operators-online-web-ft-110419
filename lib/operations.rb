96506df62c34426ee76ac528e5569ba813aa90e1
Albert LaQue <albertlaque@gmail.com>
2019-11-09 21:37:14 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,4 +1,12 @@
+#Takes in argument of speed.
+#Will return true if speed is unsafe.
+#Will return false if speed is safe.
+#will use if/else to build #unsafe?
+#
+
+
 def unsafe?(speed)
+  
 
 end
 
